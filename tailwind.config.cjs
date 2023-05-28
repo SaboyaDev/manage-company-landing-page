d3fd0c45e11fbebe3b801d6022b3da60ad1fad48
Jose L Saboya <saboyadev@gmail.com>
2023-05-28 18:59:46 -0400

    modified astro config in oder to create custom input css file and create custome css component classes. Responsive navbar layout completed, missing hamburger menu

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,36 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		// screens: {
+		// 	xsm: '375px',
+		// 	sm: '640px',
+		// 	md: '768px',
+		// 	ls: '1024px',
+		// 	xl: '1280px',
+		// 	'2xl': '1440px',
+		// },
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '976px',
+			xl: '1440px',
+		},
+		extend: {
+			colors: {
+				brightRed: 'hsl(12, 88%, 59%)',
+				brightRedLight: 'hsl(12, 88%, 69%)',
+				brightRedSupLight: 'hsl(12, 88%, 95%)',
+				darkBlue: 'hsl(228, 39%, 23%)',
+				darkGrayishBlue: 'hsl(227, 12%, 61%)',
+				veryDarkBlue: 'hsl(233, 12%, 13%)',
+				veryPaleRed: 'hsl(13, 100%, 96%)',
+				veryLightGray: 'hsl(0, 0%, 98%)',
+				light: 'hsl(9, 54%, 97%)',
+			},
+			fontFamily: {
+				vietmanPro: "'Be Vietnam Pro', sans-serif",
+			},
+		},
 	},
 	plugins: [],
 }
